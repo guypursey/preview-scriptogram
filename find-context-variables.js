9e97bb31a7828810d1f7c2b9f1b1d883810fc836
Guy Pursey <guypursey@gmail.com>
2014-11-01 20:59:30 +0000

    Rewrote function that loops over handlebar variables to account for context.
    I did this by putting the function for the `forEach` function into a closure.
    The closure contains a domain array that allows for reference of a handlebar's
    particular context. The domain in the outer scope of the looped function
    gets the context shifted/unshifted as appropriate.

--- find-context-variables.js
+++ find-context-variables.js
@@ -7,19 +7,38 @@ fs.readFile("./theme/main.html", { "encoding": "utf-8" }, function (err, data) {
 	var context_array = data.match(/\{\{(.*?)\}\}/g),
 		context_object = {};
 
-	context_array.forEach(function (v, i, a) {
-		var property = v.replace(/\{\{(.*?)\}\}/, "$1"),
-			valuetype;
-		if (!context_object.hasOwnProperty(property) && property.match(/^[#\^\/]/)) {
-			property = property.replace(/^[#\^\/]/, "");
-			valuetype = [];
-		} else if (!property.match(/^\//)) {
-			valuetype = "";
+	context_array.forEach((function (top_context) {
+		var domain = [top_context];
+		return function (v, i, a) {
+			var property = v.replace(/\{\{(.*?)\}\}/, "$1"),
+				valuetype,
+				context_object = domain[0];
+
+			// Domain shift or unshift based on block character.
+			if (property.match(/^#/)) {
+				// Create exemplary object within block.
+				valuetype = context_object[property.substr(1)] || [{}];
+				domain.unshift(valuetype[0]);
+			} else if (property.match(/^\^/)) {
+				domain.unshift(context_object);
+			} else if (property.match(/^\//)) {
+				domain.shift();
+			} else {
+				valuetype = "";
+			}
+
+			if (typeof valuetype !== "undefined") {
+				property = property.replace(/^[#\^\/]/, "");
+				if (context_object.hasOwnProperty(property)) {
+					if (typeof context_object[property] !== typeof valuetype) {
+						console.log("WARNING: Property `" + property + "` originally logged as " + typeof context_object[property] + " now found as " + typeof valuetype);
+					}
+				} else {
+					context_object[property] = valuetype;
+				}
+			}
 		}
-
-		context_object[property] = valuetype;
-
-	});
+	}(context_object)));
 
 	fs.writeFile("context-array.json", JSON.stringify(context_object, null, "\t"), {"encoding": "utf-8"});
 
